9adf374aeeee3fe56393f8dc2681a48ac0329d77
Mashry <kazi.mashry01@gmail.com>
2022-03-17 00:58:56 +0600

    added a test function to the root

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Application} from 'express';
+import express, {Application, Request, Response} from 'express';
 import cors from 'cors';
 import userRoute from './routes/userRoute';
 
@@ -10,6 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req: Request, res: Response) => res.send('Everything is working fine'))
 app.use('/user', userRoute);
 
 const port = process.env.port || 5000
