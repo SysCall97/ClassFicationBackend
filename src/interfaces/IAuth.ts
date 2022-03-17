ad5a68f9dd75a4572f15fb28b30e2a27e0ade2ce
Mashry <kazi.mashry01@gmail.com>
2022-03-17 13:39:33 +0600

    signin interface added

---
+++ src/interfaces/IAuth.ts
@@ -0,0 +1,15 @@
+export interface ISignup {
+    name: string,
+    email: string,
+    password: string
+};
+
+export interface IAuth {
+    _id: string
+};
+
+export interface ISignin {
+    email: string,
+    password: string
+}
+
