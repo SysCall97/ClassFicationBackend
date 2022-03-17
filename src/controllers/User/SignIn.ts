8000ada689a3883287c0d848b7a075ae09990130
SysCall97 <kazi.mashry01@gmail.com>
2022-03-17 16:13:37 +0600

    password protection added

--- src/controllers/User/SignIn.ts
+++ src/controllers/User/SignIn.ts
@@ -1,3 +1,4 @@
+import { INVALID_EMAIL_PASSWORD, SIGNIN_ERROR } from './../../messages/index';
 import {Request, Response} from 'express'
 import crypto from 'crypto'
 import userService from '../../services/User';
@@ -7,16 +8,20 @@ import Token from '../../services/Token';
 class SignIn {
     public static async perform(req: Request, res: Response): Promise<any> {
         try {
-            const data: IAuth = await userService.signIn({
+            const data: IAuth[] = await userService.signIn({
                 email: req.body.email,
                 password: crypto.createHash('md5').update(req.body.password).digest('hex')
             });
 
-            const token: string = Token.getToken(data);
+            if(!data.length) {
+                return res.status(404).json({ message: INVALID_EMAIL_PASSWORD });
+            }
+
+            const token: string = Token.getToken(data[0]);
             
             return res.status(200).json({ token });
         } catch (err: any) {
-            return res.status(500).json({ message: err.message || "Error occured while signup" });
+            return res.status(500).json({ message: err.message || SIGNIN_ERROR });
         }
     }
 }
