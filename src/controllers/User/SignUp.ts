18943068d2b4c577c7d3b3bc162acb9b2a1499cb
Mashry <kazi.mashry01@gmail.com>
2022-03-17 12:30:31 +0600

    seperated token service

--- src/controllers/User/SignUp.ts
+++ src/controllers/User/SignUp.ts
@@ -1,8 +1,8 @@
 import {Request, Response} from 'express'
 import crypto from 'crypto'
-import * as jwt from 'jsonwebtoken';
 import userService from '../../services/User';
 import { IAuth } from '../../interfaces';
+import Token from '../../services/Token';
 
 class SignUp {
     public static async perform(req: Request, res: Response): Promise<any> {
@@ -13,24 +13,12 @@ class SignUp {
                 password: crypto.createHash('md5').update(req.body.password).digest('hex')
             });
 
-            const token: string = SignUp.getToken(data);
+            const token: string = Token.getToken(data);
             
             return res.status(200).json({ token });
         } catch (err: any) {
             return res.status(500).json({ message: err.message || "Error occured while signup" });
         }
-    }
-    
-    public static getToken(data: IAuth): string {
-        const privateKey: string = process.env.JWT_SECRET!;
-        const expireMinute: string = process.env.JWT_EXPIRE_MINUTE!;
-        const info = {
-            uid: data._id,
-            signedIn: new Date().toLocaleString()
-        }
-        const token: string = jwt.sign(info, privateKey, { expiresIn: Number(expireMinute) * 60 });
-
-        return token;
     }
 }
 
