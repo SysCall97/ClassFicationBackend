dc8b2e378038515ff423256f4be0c0c6fbc3445d
SysCall97 <kazimashry@gmail.com>
2022-05-29 19:30:09 +0600

    registered the new validation in the index file

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
+import { validateRole } from './roleValidation';
 import { validateName } from './nameValidation';
 import { validatePassword } from './passwordValidation';
 import { validateEmail } from './emailValidation';
@@ -5,5 +6,5 @@ import { signUpObjectValidation } from './signUpObjectValidation';
 import { signInObjectValidation } from './signInObjectValidation';
 
 export {
-    validateEmail, signUpObjectValidation, validatePassword, validateName, signInObjectValidation
+    validateEmail, signUpObjectValidation, validatePassword, validateName, signInObjectValidation, validateRole
 }
