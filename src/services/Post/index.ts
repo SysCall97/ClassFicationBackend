30401dd0fee30820f36ae3590a40ecc088fbe8ee
SysCall97 <kazimashry@gmail.com>
2022-05-31 23:30:16 +0600

    sort added in get post API

--- src/services/Post/index.ts
+++ src/services/Post/index.ts
@@ -1,4 +1,5 @@
 import { ICheckEntityOwner, ICreatePost, IGetPost } from "../../interfaces";
+import Comment from "../../models/Comment";
 import Post from "../../models/Post";
 import User from "../../models/User";
 
@@ -7,7 +8,7 @@ class PostService {
         return new Promise(async (resolve, reject) => {
             try {
                 await Post.create({classCode: data.classCode, post: data.post, uid: data.uid, commentIds: []});
-                resolve({...data, commentIds: []});
+                resolve({...data, comments: []});
             } catch (error) {
                 reject(error);
             }
@@ -18,14 +19,22 @@ class PostService {
         return new Promise(async (resolve, reject) => {
             try {
                 let posts;
-                if(data.role === 2) posts = await Post.find({classCode: data.classCode, active: true});
-                else posts = await Post.find({classCode: data.classCode, uid: data.uid, active: true});
+                if(data.role === 2) posts = await Post.find({classCode: data.classCode, active: true}).sort({ createdAt : -1});
+                else posts = await Post.find({classCode: data.classCode, uid: data.uid, active: true}).sort({ createdAt : -1});
+
                 let _posts: any[] = await Promise.all(posts.map(async post => {
                     const user = await User.findById(post.uid).select('name');
                     post.userName = user?.name;
-                    const obj = {uid: post.uid, userName: user?.name, _id: post._id, classCode: post.classCode, post: post.post, commentIds: post.commentIds};
+                    const commentIds = post.commentIds;
+                    const comments = await Promise.all(commentIds.map(async (commentId: any) => {
+                        const comment = await Comment.findById(commentId);
+                        const user = await User.findById(comment.uid).select('name');
+                        return { commentId: comment._id, comment: comment.comment, userName: user.name };
+                    }));
+                    const obj = {uid: post.uid, userName: user?.name, _id: post._id, classCode: post.classCode, post: post.post, comments};
                     return obj;
                 }));
+                
                 resolve(_posts);
             } catch (error) {
                 reject(error);
