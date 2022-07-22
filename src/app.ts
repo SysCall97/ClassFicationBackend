1cd719439c6c9cd4b70bca644d517dd49db24962
SysCall97 <kazimashry@gmail.com>
2022-07-22 10:58:14 +0600

    worked added to run multiple instance of app

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import authRoute from './routes/authRoute';
 import { MongoDB } from './database';
 import { IDb } from './interfaces';
 import seedRoute from './routes/seedRoute';
+import cluster from 'cluster';
+import { cpus } from 'os';
 
 require('dotenv').config();
 
@@ -23,8 +25,24 @@ const startApp = () => {
     
     const port = process.env.PORT || 5000;
     
-    app.listen(port, () => console.log("Server is running"));
+    app.listen(port, () => console.log(`[server]: Server is running at http://localhost:${port}`));
 }
 
-const db: IDb = new MongoDB();
-db.connect(startApp);
+
+if(cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    const numCPUs = cpus().length;
+
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+    })
+} else {
+    const db: IDb = new MongoDB();
+    db.connect(startApp);
+    console.log(`Worker ${process.pid} started`);
+}
+
