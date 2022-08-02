4acf23b7d50143a64fbae7a6f9437ed5e1e14af1
SysCall97 <kazimashry@gmail.com>
2022-08-03 02:00:56 +0600

    added new API to check token validity

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,12 +1,15 @@
+import { StatusCodes } from 'http-status-codes';
 // dependencies
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
 import { SignIn, SignOut } from '../controllers/user';
-import { signInMiddleware } from '../middleware';
+import { authenticateMiddleware, signInMiddleware } from '../middleware';
+import { TOKEN_STILL_ALIVE } from '../messages';
 
 const authRoute: Router = Router();
 
 // Routes
 authRoute.post('/signin', signInMiddleware, SignIn.perform);
 authRoute.post('/signout', SignOut.perform);
+authRoute.get('/check_token', authenticateMiddleware, (req: Request, res: Response) => res.status(StatusCodes.ACCEPTED).json({ message: TOKEN_STILL_ALIVE }));
 
 export default authRoute;
