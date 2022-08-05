8175960f59acfb70e10fddc6f786e8b51ce3e9a0
SysCall97 <kazimashry@gmail.com>
2022-08-06 00:13:33 +0600

    controller registered

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -2,5 +2,6 @@ import SignIn from "./SignIn";
 import SignOut from "./SignOut";
 import SignUp from "./SignUp";
 import UserUpdate from "./UserUpdate";
+import UserJoinedClassDetails from "./UserJoinedClassDetails";
 
-export { SignUp, SignIn, SignOut, UserUpdate };
+export { SignUp, SignIn, SignOut, UserUpdate, UserJoinedClassDetails };
