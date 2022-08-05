32f26be21a52c1358389d86e139eff0662d8ba18
SysCall97 <kazimashry@gmail.com>
2022-08-06 00:14:08 +0600

    router added to get details of class list an user joined

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,6 +1,6 @@
 // dependencies
 import {Router} from 'express';
-import { SignUp, UserUpdate } from '../controllers/user';
+import { SignUp, UserJoinedClassDetails, UserUpdate } from '../controllers/user';
 import { authenticateMiddleware, signUpMiddleware } from '../middleware';
 
 const userRoute: Router = Router();
@@ -8,5 +8,6 @@ const userRoute: Router = Router();
 // Routes
 userRoute.post('/signup', signUpMiddleware, SignUp.perform);
 userRoute.put('/update', authenticateMiddleware, UserUpdate.perform);
+userRoute.get('/class_list', authenticateMiddleware, UserJoinedClassDetails.perform)
 
 export default userRoute;
