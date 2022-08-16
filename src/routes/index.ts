5c6e809757a9c88876b8dff51eef73851b136b40
SysCall97 <kazimashry@gmail.com>
2022-08-17 01:28:59 +0600

    added file to test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,24 @@ import seedRoute from "./seedRoute";
 import userRoute from "./userRoute";
 import classRoute from './classRoute';
 
+import fs from 'fs';
+import { Request, Response} from 'express';
+
 
 export const router: Router = Router();
 router.use('/seed', seedRoute)
 router.use('/user', userRoute);
 router.use('/auth', authRoute);
 router.use('/class', authenticateMiddleware, classRoute);
+
+router.get('/download', (req: Request, res: Response) => {
+    const file = process.cwd() + "/public/OOP-course-payment.pdf";
+    fs.readFile(file, (err, content) => {
+        if(err) {
+            res.writeHead(404, {'content-type': 'text/html'});
+            res.end("<h1>No Such file</h1>");
+        }
+        res.writeHead(200, {'content-type': 'application/pdf'});
+        res.end(content);
+    });
+});
