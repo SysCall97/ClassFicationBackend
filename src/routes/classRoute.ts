01ed9bff7027e2921101cec7e38c96b9b4d40b85
SysCall97 <kazimashry@gmail.com>
2022-08-17 18:56:15 +0600

    added new route and used filter out middleware to create class API

--- src/routes/classRoute.ts
+++ src/routes/classRoute.ts
@@ -1,5 +1,6 @@
 // dependencies
 import {Router} from 'express';
+import CreateAssignment from '../controllers/_class/assignment/CreateAssignmnt';
 import CreateClass from '../controllers/_class/CreateClass';
 import JoinClass from '../controllers/_class/JoinClass';
 import CreateComment from '../controllers/_class/post/comment/CreateComment';
@@ -12,19 +13,24 @@ import EditPost from '../controllers/_class/post/EditPost';
 import GetPost from '../controllers/_class/post/GetPost';
 import GetStudents from '../controllers/_class/students/GetStudents';
 import GetTeachers from '../controllers/_class/teachers/GetTeachers';
+import { uploadType } from '../helpers';
 import { 
     checkClassExistsMiddleware, 
     checkCommentOwnerMiddleware, 
     checkJoinedClassMiddleware, 
     checkPostExistsMiddleware, 
-    checkPostOwnerMiddleware 
+    checkPostOwnerMiddleware,
+    filterOutTeacher,
+    filterOutStudent,
+    multerGetter,
+    authenticateMiddleware
 } from '../middleware';
 import { checkCommentExistsMiddleware } from '../middleware/checkCommentExist';
 
 const classRoute: Router = Router();
 
 // Routes
-classRoute.post('/create', CreateClass.perform);
+classRoute.post('/create', filterOutTeacher, CreateClass.perform);
 classRoute.put('/:class_code/join', checkClassExistsMiddleware, JoinClass.perform);
 
 classRoute.post('/:class_code/post', checkClassExistsMiddleware, checkJoinedClassMiddleware, CreatePost.perform);
@@ -39,5 +45,10 @@ classRoute.delete('/:class_code/post/:post_id/comment/:comment_id', checkClassEx
 
 classRoute.get('/:class_code/students', checkClassExistsMiddleware, checkJoinedClassMiddleware, GetStudents.perform);
 classRoute.get('/:class_code/teachers', checkClassExistsMiddleware, checkJoinedClassMiddleware, GetTeachers.perform);
+
+// here we have to use authenticateMiddleware after multer 
+// though we're using authenticateMiddleware before any route here, but we're using it twice in this API
+// It's because multer is overriding the req.body attribute that's been set in the authenticateMiddleware
+classRoute.post('/:class_code/assignment', filterOutStudent, checkClassExistsMiddleware, checkJoinedClassMiddleware, multerGetter(uploadType.assignment).single('file'), authenticateMiddleware, CreateAssignment.perform);
 
 export default classRoute;
