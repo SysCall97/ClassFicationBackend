8f674461724d83f811d1e75284ead78f64005f37
SysCall97 <kazimashry@gmail.com>
2022-10-01 11:34:42 +0600

    added different methods to submission service

--- src/services/Submission/index.ts
+++ src/services/Submission/index.ts
@@ -1,8 +1,8 @@
 import Assignment from '../../models/Assignment';
-import Map from '../../models/Map'
-import { ISaveSubmission } from './../../interfaces/IClass';
+import { ISaveSubmission, IUpdateMark } from './../../interfaces/IClass';
 import Submission from '../../models/Submission';
 import mongoose from "mongoose";
+import { TRY_AGAIN_LATER } from '../../messages';
 
 class SubmissionService {
     public static saveSubmission(data: ISaveSubmission): Promise<any> {
@@ -24,6 +24,32 @@ class SubmissionService {
                 reject(error);
             }
         });
+    }
+
+    public static updateSubmissionMark(data: IUpdateMark): Promise<any> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const { submissionId, mark } = data;
+                await Submission.findByIdAndUpdate({_id: submissionId}, {mark: mark});
+                resolve("SUCCESS");
+            } catch (error: any) {
+                reject(error);
+            }
+        });
+    }
+
+    public static async getSubmissionCodeById(id: string): Promise<any> {
+        try {
+            const assignmentCode = await Submission.findOne({_id: id}).select('submissionCode student -_id');
+            return assignmentCode;
+        } catch (error) {
+            return new Error(TRY_AGAIN_LATER);
+        }
+    }
+
+    public static async isAlreadySubmitted(uid: string, assignmentCode: string): Promise<boolean> {
+        const count = await Submission.find({ student: uid, assignmentCode: assignmentCode}).count();
+        return count > 0;
     }
 }
 
