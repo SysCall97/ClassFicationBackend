4a482e7ed595514fe045b18fa87ae921bbe16870
SysCall97 <kazimashry@gmail.com>
2022-11-21 14:05:27 +0600

    sessionRoute registered

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import authRoute from "./authRoute";
 import seedRoute from "./seedRoute";
 import userRoute from "./userRoute";
 import classRoute from './classRoute';
+import sessionRoute from "./sessionRoute";
 
 
 export const router: Router = Router();
@@ -11,3 +12,4 @@ router.use('/seed', seedRoute)
 router.use('/user', userRoute);
 router.use('/auth', authRoute);
 router.use('/class', authenticateMiddleware, classRoute);
+router.use('/session', sessionRoute);
